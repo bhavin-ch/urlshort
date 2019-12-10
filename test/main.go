bfaed607965d0a0de61d062dbc6e1835edf2f5a3
Bhavin <bhavin137@gmail.com>
2019-12-10 13:26:35 +0530

    more scaffold

--- test/main.go
+++ test/main.go
@@ -8,6 +8,23 @@ import (
 
 func main() {
 	mux := defaultMux()
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	yaml := `
+- path: /urlshort
+  url: https://github.com/bhavin-ch/urlshort
+- path: /urlshort-final
+  url: https://github.com/bhavin-ch/urlshort/tree/solution
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
